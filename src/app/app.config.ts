db93139b3b45fb74b66b3c238a4f9074e1f206c6
Avinash Suryawanshi <suryawanshiavinash18@gmail.com>
2025-07-06 02:27:02 +0530

    Spinner changes done

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,4 +1,4 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
@@ -6,6 +6,7 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 import { provideNativeDateAdapter } from '@angular/material/core';
 import { authInterceptor } from './shared/interceptor/auth.interceptor';
+import { NgxSpinnerModule } from 'ngx-spinner';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync(),
@@ -15,5 +16,6 @@ export const appConfig: ApplicationConfig = {
     ),
     provideNativeDateAdapter(),
     provideRouter(routes),
+    importProvidersFrom(NgxSpinnerModule),
   ]
 };
