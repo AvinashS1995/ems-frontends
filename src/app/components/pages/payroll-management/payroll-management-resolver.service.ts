f0903f4077714ce323810822a34739d4c139427a
Avinash Suryawanshi <suryawanshiavinash18@gmail.com>
2026-08-16 15:04:08 +0530

    changes done

--- src/app/components/pages/payroll-management/payroll-management-resolver.service.ts
+++ src/app/components/pages/payroll-management/payroll-management-resolver.service.ts
@@ -2,19 +2,25 @@ import { Injectable } from '@angular/core';
 import { ApiService } from '../../../shared/service/api/api.service';
 import { forkJoin, Observable, of } from 'rxjs';
 import { API_ENDPOINTS } from '../../../shared/common/api-contant';
+import { CommonService } from '../../../shared/service/common/common.service';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PayrollManagementResolverService {
-  constructor(private apiService: ApiService) {}
+  constructor(
+    private apiService: ApiService,
+    private commonService: CommonService,
+  ) {}
 
   resolve(): Observable<any> {
     let getAllEmployee = of({});
-
+    const payload = {
+      empNo: this.commonService.getCurrentUserDetails()?.empNo || '',
+    };
     getAllEmployee = this.apiService.postApiCall(
       API_ENDPOINTS.SERVICE_GET_USER_LIST,
-      {}
+      payload,
     );
 
     return forkJoin({
